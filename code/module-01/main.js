83e01192fd89c99fa75489202a17661d5716629e
maryt <maryt@skillup.tech>
2020-12-05 10:53:59 -0800

    minor edits for consistency

--- code/module-01/main.js
+++ code/module-01/main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
     data() {
         return {
             //TODO: Define a data property item as product_name with a text value
-            product_name: 'Book a Cruise to the Moon'
+            product_name: 'Book a Cruise to the Moon',
+            //Coding Challenge: Define a data property item as product_name with a text value
+            product_desc: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
         }
     },
 })
