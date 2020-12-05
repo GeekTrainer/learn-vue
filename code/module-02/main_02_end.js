0e0e239c5eb8d86690220260ed1e9a7c85343b05
maryt <maryt@skillup.tech>
2020-12-05 12:37:08 -0800

    add code files and update TODO narrative for consistency

--- code/module-02/main_02_end.js
+++ code/module-02/main_02_end.js
@@ -2,12 +2,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            //TODO: Define a data property item as product_name with a text value
             product_name: 'Book a Cruise to the Moon',
-            //Coding Challenge: Define a data property item as product_name with a text value
             product_desc: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            //TODO: Add a property with a value for the currently selected image
+            selected_prod_img: './assets/images/asteroid-5737398_1284x856.jpg',
+            //Coding Challenge: Add a property with a value for the currently selected image
+            company_url: 'https://docs.microsoft.com/en-us/learn/',
         }
-    },
+    }
 })
 
 // Define another constant to mount the Vue app to the DOM
