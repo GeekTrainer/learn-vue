4d4b7d90a66ff4569bf1275406f16eccb7d2c6a5
maryt <maryt@skillup.tech>
2021-01-04 12:56:57 -0800

    updates to styles

--- code/module-08/m08_end/components/ReviewForm.js
+++ code/module-08/m08_end/components/ReviewForm.js
@@ -8,7 +8,7 @@ app.component('review-form', {
   template: 
   /*html*/
   `<div class="componentBox">
-    <h4 style="text-align:center;">{{ rateTrip }}</h4>
+    <h4 style="text-align:center;">{{ rateTrip }} {{ previous }}</h4>
     <p v-if="previous">Please tell us about your latest trip with Relecloud Galaxy Tours.</p>
     <p v-else>Please provide comments or submit questions regarding Relecloud Galaxy Tours.</p>
   </div>`,
